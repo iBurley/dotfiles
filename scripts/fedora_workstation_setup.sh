29c2f532851197e478a8a0b4122106b1d0a040b1
iBurley <iBurley@live.com>
2021-05-30 18:39:07 -0400

    Added gsettings command to disable extensions (again)

--- scripts/fedora_workstation_setup.sh
+++ scripts/fedora_workstation_setup.sh
@@ -36,6 +36,7 @@ gsettings set org.gnome.gedit.preferences.editor insert-spaces true
 gsettings set org.gnome.gedit.preferences.editor tabs-size 2
 gsettings set org.gnome.nautilus.icon-view default-zoom-level 'standard'
 gsettings set org.gnome.settings-daemon.plugins.media-keys max-screencast-length 0
+gsettings set org.gnome.shell disable-user-extensions true
 gsettings set org.gtk.Settings.FileChooser sort-directories-first true
 
 # speed up dnf
