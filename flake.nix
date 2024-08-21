4a023b424e71fb9af1934d10e0be957366dccf77
iBurley <iBurley@live.com>
2024-08-21 00:42:36 -0400

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+
+  description = "NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+    nixosConfigurations = {
+      desktop = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.iburley = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+  
+}
