fbbec31823f17978c355e6773f3b579cbb249dec
iBurley <iBurley@live.com>
2024-08-24 19:39:24 -0400

    Gave up on Plymouth

--- configuration.nix
+++ configuration.nix
@@ -7,28 +7,12 @@
       ./hardware-configuration.nix
     ];
 
-  #boot.loader.systemd-boot.enable = true;
-  #boot.loader.efi.canTouchEfiVariables = true;
-
   boot = {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-    plymouth.enable = true;
-    plymouth.theme = "bgrt";
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-    ];
     loader.timeout = 0;
   };
-  
+
   networking.hostName = "desktop";
   networking.networkmanager.enable = true;
 
