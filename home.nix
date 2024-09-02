4a46420567056c90e42c3028d362f9ebc6a17f28
iBurley <iBurley@live.com>
2024-09-01 21:27:13 -0400

    Simplify Firefox setup

--- home.nix
+++ home.nix
@@ -93,18 +93,10 @@
       OverrideFirstRunPage = "";
       OverridePostUpdatePage = "";
       SearchBar = "separate";
-      ExtensionSettings = {
-        # Adguard
-        "adguardadblocker@adguard.com" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/adguard-adblocker/latest.xpi";
-          installation_mode = "normal_installed";
-        };
-        # Bitwarden
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-          installation_mode = "normal_installed";
-        };
-      };
+      Extensions.Install = [
+        "https://addons.mozilla.org/firefox/downloads/latest/adguard-adblocker/latest.xpi"
+        "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi"
+      ];
       Preferences = { 
         "layers.acceleration.force-enabled" = true;
         "media.hardware-video-decoding.force-enabled" = true;
