15be0bdbdc4edcab0f75fbe1359f57e956509193
iBurley <iBurley@live.com>
2024-09-01 20:03:16 -0400

    Fixed comment (OCD)

--- home.nix
+++ home.nix
@@ -99,7 +99,7 @@
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/adguard-adblocker/latest.xpi";
           installation_mode = "normal_installed";
         };
-        #Bitwarden
+        # Bitwarden
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
           installation_mode = "normal_installed";
