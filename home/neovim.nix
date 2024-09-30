c49c9c67f4660b042fe71bca983c04b2af37cdef
iBurley <iBurley@live.com>
2024-09-30 00:58:09 -0400

    Create neovim.nix

---
+++ home/neovim.nix
@@ -0,0 +1,64 @@
+{ config, pkgs, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    defaultEditor = true;
+    extraLuaConfig = ''
+      -- general
+      vim.opt.expandtab = true
+      vim.opt.shiftwidth = 2
+      vim.opt.softtabstop = 2
+      vim.opt.tabstop = 2
+      vim.opt.number = true
+      vim.opt.cursorline = true
+      vim.g.mapleader = " "
+
+      -- colorscheme
+      require("kanagawa").load("wave")
+
+      -- statusline
+      require('lualine').setup()
+
+      -- treesitter
+      local treesitter_path = vim.fn.expand("$HOME/.local/share/nvim/treesitter")
+      vim.opt.runtimepath:append(treesitter_path)
+      require('nvim-treesitter.configs').setup {
+        parser_install_dir = treesitter_path,
+        highlight = { enable = true },
+        indent = { enable = true }
+      }
+
+      -- autopairs
+      require('nvim-autopairs').setup()
+
+      -- telescope
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    '';
+    extraPackages = with pkgs; [
+      gcc
+      git
+    ];
+    plugins = with pkgs.vimPlugins; [
+      kanagawa-nvim
+      lualine-nvim
+      nvim-autopairs
+      nvim-treesitter.withAllGrammars
+      telescope-file-browser-nvim
+      telescope-nvim
+    ];
+  };
+
+  xdg.desktopEntries."nvim" = {
+    name = "Neovim wrapper";
+    noDisplay = true;
+  };
+
+}
