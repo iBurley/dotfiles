6b5dda0c48614be64e9ffae884874260d5fc5dc0
iBurley <iBurley@live.com>
2024-10-03 22:49:00 -0400

    Create stylix.nix

---
+++ system/stylix.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+{
+
+  stylix = {
+    enable = true;
+    image = ./wallpaper.png;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/kanagawa.yaml";
+    fonts = {
+      serif = {
+        name = "Cantarell";
+        package = pkgs.cantarell-fonts;
+      };
+      sansSerif = {
+        name = "Cantarell";
+        package = pkgs.cantarell-fonts;
+      };
+      monospace = {
+        name = "SauceCodePro NFM Semibold 10";
+        package = pkgs.nerdfonts.override { fonts = [ "SourceCodePro" ]; };
+      };
+      emoji = {
+        name = "Noto Color Emoji";
+        package = pkgs.noto-fonts-emoji;
+      };
+    };
+  };
+
+}
