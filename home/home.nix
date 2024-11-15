ee0b8530fe91d465dcb78bb6012c10055ed79082
iBurley <iBurley@live.com>
2024-11-14 21:30:25 -0500

    Move packages to packages.nix

--- home/home.nix
+++ home/home.nix
@@ -1,5 +1,3 @@
-{ pkgs, pkgs-unstable,  ... }:
-
 {
 
   imports = [
@@ -8,48 +6,11 @@
     ./mpv.nix
     ./nautilus-scripts.nix
     ./neovim.nix
+    ./packages.nix
   ];
 
   home.username = "iburley";
   home.homeDirectory = "/home/iburley";
-
-  home.packages = (with pkgs; [
-    discord
-    epiphany
-    evince
-    ffmpeg
-    fragments
-    gnome-console
-    gnome-text-editor
-    gnome.file-roller
-    gnome.gnome-boxes
-    gnome.gnome-calculator
-    gnome.gnome-calendar
-    gnome.gnome-clocks
-    gnome.gnome-disk-utility
-    gnome.gnome-font-viewer
-    gnome.gnome-logs
-    gnome.gnome-music
-    gnome.gnome-system-monitor
-    gnome.nautilus
-    gnome.simple-scan
-    imagemagick
-    libnotify
-    libreoffice
-    loupe
-    mousai
-    (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
-    newsflash
-    noto-fonts-emoji
-    prismlauncher
-    spotify
-    xclip
-    yt-dlp
-  ]) ++ (with pkgs-unstable; [
-    (alpaca.override { ollama = ollama-rocm; })
-    celluloid
-    zed-editor
-  ]);
 
   xdg.configFile."user-dirs.dirs".force = true;
   xdg.userDirs = {
