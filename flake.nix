8bb09bb47adedaee4f5b0fdbcafd8c7a95d8be69
iBurley <iBurley@live.com>
2024-12-07 01:28:37 -0500

    update to 24.11

--- flake.nix
+++ flake.nix
@@ -4,14 +4,14 @@
 
   inputs = {
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nautilus-scripts = {
       url = "github:iBurley/nautilus-scripts";
       flake = false;
     };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
