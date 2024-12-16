e014ebb825a88dd8306003405c39c97abef17f47
iBurley <iBurley@live.com>
2024-12-16 17:20:58 -0500

    removed unecessary UEFI workaround

--- system/virtualization.nix
+++ system/virtualization.nix
@@ -7,19 +7,6 @@
     spiceUSBRedirection.enable = true;
   };
 
-  # Workaround to get UEFI working in GNOME Boxes
-  # Hopefully temporary
-  systemd.tmpfiles.rules =
-  let
-    firmware =
-      pkgs.runCommandLocal "qemu-firmware" { } ''
-        mkdir $out
-        cp ${pkgs.qemu}/share/qemu/firmware/*.json $out
-        substituteInPlace $out/*.json --replace ${pkgs.qemu} /run/current-system/sw
-      '';
-  in
-  [ "L+ /var/lib/qemu/firmware - - - - ${firmware}" ];
-
   users.users.iburley = { extraGroups = [ "libvirtd" ]; };
 
 }
