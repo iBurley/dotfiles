4965cdaeab4b70470aa5570812c0affa5531abd6
iBurley <iBurley@live.com>
2025-02-06 17:49:22 -0500

    Undo mistake

--- home/packages.nix
+++ home/packages.nix
@@ -1,83 +1,43 @@
-{ config, lib, pkgs, pkgs-unstable, ... }:
+{ pkgs, pkgs-unstable, ... }:
 
 {
 
-  imports = [
-    ./hardware-configuration.nix
-    ./steam.nix
-    ./virtualization.nix
-  ];
-
-  boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
-    loader.efi.canTouchEfiVariables = true;
-    loader.systemd-boot.enable = true;
-    loader.timeout = 0;
-    tmp.cleanOnBoot = true;
-  };
-
-  systemd.services.NetworkManager-wait-online.enable = false;
-
-  networking = {
-    hostName = "desktop";
-    networkmanager.enable = true;
-  };
-
-  time.timeZone = "America/New_York";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  services.xserver = {
-    enable = true;
-    desktopManager.gnome.enable = true;
-    displayManager.gdm.enable = true;
-  };
-  services.gnome.core-utilities.enable = false;
-
-  services.printing.enable = true;
-
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  users.users.iburley = {
-    description = "iBurley";
-    extraGroups = [ "networkmanager" "wheel" ];
-    isNormalUser = true;
-  };
-
-  services.xserver.excludePackages = with pkgs; [
-    xterm
-  ];
-
-  nix.gc = {
-    automatic = true;
-    dates = "daily";
-    options = "--delete-older-than 7d";
-    persistent = true;
-  };
-
-  nix.settings = {
-    auto-optimise-store = true;
-    experimental-features = [ "nix-command" "flakes" ];
-  };
-
-  system.stateVersion = "24.05";
+  home.packages = (with pkgs; [
+    (alpaca.override { ollama = ollama-rocm; })
+    bottles
+    discord
+    epiphany
+    evince
+    ffmpeg
+    file-roller
+    fragments
+    gnome-boxes
+    gnome-calculator
+    gnome-calendar
+    gnome-clocks
+    gnome-console
+    gnome-disk-utility
+    gnome-font-viewer
+    gnome-logs
+    gnome-music
+    gnome-system-monitor
+    gnome-text-editor
+    imagemagick
+    libnotify
+    libreoffice
+    loupe
+    nautilus
+    (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
+    newsflash
+    noto-fonts-emoji
+    prismlauncher
+    simple-scan
+    spotify
+    xclip
+    yt-dlp
+    zed-editor
+  ]) ++ (with pkgs-unstable; [
+    # unstable packages here
+  ]);
 
 }
