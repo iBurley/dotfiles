0bbd04d93947b5d0f2a2cc382fcaf18bf3e684cd
iBurley <iBurley@live.com>
2025-04-27 20:21:12 -0400

    Added 'noatime' to drive options

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -1,4 +1,4 @@
-{ config, lib, modulesPath, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 
 {
 
@@ -16,24 +16,25 @@
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/ece2361d-660c-4e0b-8c62-b5317705ec02";
     fsType = "ext4";
-    options = [ "discard" ];
+    options = [ "discard" "noatime" ];
   };
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/3ACB-F43E";
     fsType = "vfat";
-    options = [ "fmask=0022" "dmask=0022" "discard" ];
+    options = [ "fmask=0022" "dmask=0022" "discard" "noatime" ];
   };
 
   fileSystems."/mnt/Storage" = {
     device = "/dev/disk/by-uuid/05044ab0-bb11-47ff-b2f3-881d3bc4ac09";
     fsType = "ext4";
+    options = [ "noatime" ];
   };
 
   fileSystems."/mnt/Games" = {
     device = "/dev/disk/by-uuid/a9e88f8e-0fab-47eb-a095-43c1aa17dfb6";
     fsType = "ext4";
-    options = [ "discard" ];
+    options = [ "discard" "noatime" ];
   };
 
   swapDevices = [ ];
