c149fb48d1db817c228b36f4a447b3db14641235
iBurley <iBurley@live.com>
2025-05-20 03:17:57 -0400

    updated to nixpkgs 25.05

--- flake.nix
+++ flake.nix
@@ -4,43 +4,47 @@
 
   inputs = {
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nautilus-scripts = {
       url = "github:iBurley/nautilus-scripts";
       flake = false;
     };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, home-manager, nautilus-scripts, nixpkgs, nixpkgs-unstable, ... }: {
-    nixosConfigurations = {
-      desktop = nixpkgs.lib.nixosSystem rec {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit nautilus-scripts;
-          pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          };
-          pkgs-unstable = import nixpkgs-unstable {
-            inherit system;
-            config.allowUnfree = true;
-          };
+  outputs = { self, home-manager, nautilus-scripts, nixpkgs, nixpkgs-unstable, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      pkgs-unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      nixosConfigurations = {
+        desktop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./system/configuration.nix
+            home-manager.nixosModules.home-manager {
+              nixpkgs.pkgs = pkgs;
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.iburley = import ./home/home.nix;
+              home-manager.extraSpecialArgs = {
+                inherit nautilus-scripts pkgs-unstable;
+              };
+            }
+          ];
         };
-        modules = [
-          ./system/configuration.nix
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.iburley = import ./home/home.nix;
-            home-manager.extraSpecialArgs = specialArgs;
-          }
-        ];
       };
     };
-  };
 
 }
