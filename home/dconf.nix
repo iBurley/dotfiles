ac67ddfef7dd52f9dcefad108a9703290a2d8334
iBurley <iburley@live.com>
2025-08-09 22:58:41 -0400

    change monospace font size

--- home/dconf.nix
+++ home/dconf.nix
@@ -37,7 +37,7 @@
       color-scheme = "prefer-dark";
       font-antialiasing = "rgba";
       font-hinting = "slight";
-      monospace-font-name = "MonaspiceAr Nerd Font Mono Medium 10";
+      monospace-font-name = "MonaspiceAr Nerd Font Mono Medium 11";
     };
     "org/gnome/desktop/peripherals/mouse" = {
       accel-profile = "flat";
