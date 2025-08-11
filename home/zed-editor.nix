cc321617042cd2af5fe037946d5e8736e6a102e2
iBurley <iburley@live.com>
2025-08-10 21:36:28 -0400

    Create zed-editor.nix

---
+++ home/zed-editor.nix
@@ -0,0 +1,88 @@
+{ pkgs, pkgs-unstable, ... }:
+
+{
+
+  programs.zed-editor = {
+    enable = true;
+    package = pkgs-unstable.zed-editor;
+    extensions = [ "docker-compose" "html" "kanagawa" "nix" ];
+    extraPackages = [ pkgs.nixd ];
+    themes = [ "kanagawa" ];
+    userSettings = {
+      theme = {
+        mode = "system";
+        light = "One Light";
+        dark = "Kanagawa";
+      };
+      buffer_font_family = "MonaspiceAr Nerd Font Mono";
+      buffer_font_size = 12;
+      buffer_font_weight = 500;
+      buffer_line_height = "standard";
+      ui_font_family = "MonaspiceAr Nerd Font Mono";
+      ui_font_weight = 500;
+      ui_font_size = 14;
+      # vim_mode = true;
+      toolbar = {
+        breadcrumbs = false;
+        quick_actions = false;
+        selections_menu = false;
+        agent_review = false;
+      };
+      title_bar = {
+        show_onboarding_banner = false;
+        show_user_picture = false;
+        show_sign_in = false;
+      };
+      gutter = {
+        runnables = false;
+        breakpoints = false;
+        folds = false;
+        min_line_number_digits = 1;
+      };
+      indent_guides = {
+        enabled = false;
+      };
+      project_panel = {
+        indent_guides = {
+          show = "never";
+        };
+      };
+      collaboration_panel = {
+        button = false;
+      };
+      chat_panel = {
+        button = "never";
+      };
+      notification_panel = {
+        button = false;
+      };
+      show_call_status_icon = false;
+      tabs = {
+        git_status = true;
+        file_icons = true;
+      };
+      tab_size = 2;
+      disable_ai = true;
+      terminal = {
+        toolbar = {
+          breadcrumbs = false;
+        };
+        font_weight = 500;
+      };
+      auto_install_extensions = {
+        docker-compose = true;
+        html = true;
+        nix = true;
+      };
+      languages = {
+        Nix = {
+          language_servers = [ "!nil" "nixd" ];
+        };
+      };
+      debugger = {
+        button = false;
+      };
+    };
+  };
+
+}
