e8317972988f694fab2db38f61778370352f4e80
iBurley <iburley@live.com>
2025-08-19 17:21:45 -0400

    restructured slightly

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
 
-  description = "NixOS Configuration";
-
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,16 +11,14 @@
       url = "github:iBurley/nautilus-scripts";
       flake = false;
     };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs =
     {
-      home-manager,
-      nautilus-scripts,
       nixpkgs,
       nixpkgs-unstable,
+      home-manager,
+      nautilus-scripts,
       ...
     }:
     let
@@ -35,23 +33,23 @@
       };
     in
     {
-      nixosConfigurations = {
-        desktop = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./system/configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.iburley = import ./home/home.nix;
-              home-manager.extraSpecialArgs = {
+      nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./system/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.iburley = import ./home/home.nix;
+              extraSpecialArgs = {
                 inherit nautilus-scripts pkgs-unstable;
               };
-              nixpkgs.pkgs = pkgs;
-            }
-          ];
-        };
+            };
+            nixpkgs.pkgs = pkgs;
+          }
+        ];
       };
     };
 
