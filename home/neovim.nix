c233a2155fe59c21a8ea18bcbea287b45f90a871
iBurley <10081177+iBurley@users.noreply.github.com>
2025-09-16 05:03:14 -0400

    set up lua_ls and stylua, new keymaps, general cleanup

--- home/neovim.nix
+++ home/neovim.nix
@@ -9,81 +9,165 @@
     vimdiffAlias = true;
     defaultEditor = true;
     extraLuaConfig = ''
-      -- map leader
+      -- LEADER KEYS
       vim.g.mapleader = " "
       vim.g.maplocalleader = " "
 
-      -- basic settings
+      -- BASIC SETTINGS
+      vim.opt.backup = false
+      vim.opt.clipboard = 'unnamedplus'
       vim.opt.cursorline = true
       vim.opt.expandtab = true
+      vim.opt.hlsearch = true
       vim.opt.ignorecase = true
+      vim.opt.incsearch = true
       vim.opt.number = true
       vim.opt.relativenumber = true
       vim.opt.scrolloff = 10
       vim.opt.shiftwidth = 2
       vim.opt.showmode = false
+      vim.opt.signcolumn = "yes"
       vim.opt.smartcase = true
       vim.opt.smartindent = true
       vim.opt.softtabstop = 2
+      vim.opt.swapfile = false
       vim.opt.tabstop = 2
+      vim.opt.termguicolors = true
       vim.opt.undofile = true
       vim.opt.wrap = false
 
-      -- window nav
-      vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
-      vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
-      vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
-      vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+      -- KEYMAPS
+      local keymap = vim.keymap.set
+      keymap("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+      keymap("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+      keymap("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+      keymap("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+      keymap("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Toggle file tree" })
+      keymap("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostic details" })
+      keymap("n", "<leader>ff", require("fzf-lua").files, { desc = "Find files" })
+      keymap("n", "<leader>fb", require("fzf-lua").buffers, { desc = "Find buffers" })
+      keymap("n", "<leader>fw", require("fzf-lua").live_grep, { desc = "Live grep search" })
+      keymap("n", "<leader>fc", require("fzf-lua").grep_cword, { desc = "Search word under cursor" })
+      keymap("n", "<leader>fd", require("fzf-lua").diagnostics_document, { desc = "Find diagnostics in file" })
+      keymap("n", "<leader>gc", require("fzf-lua").git_commits, { desc = "Git commits" })
 
-      -- window split
-      vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically" })
-      vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally" })
+      -- SPLITS
+      keymap("n", "<leader>h", function()
+        vim.cmd("leftabove vsplit")
+        require("fzf-lua").files()
+      end, { desc = "Split left and open file picker" })
 
-      -- auto pairs
-      require('nvim-autopairs').setup()
+      keymap("n", "<leader>j", function()
+        vim.cmd("rightbelow split")
+        require("fzf-lua").files()
+      end, { desc = "Split down and open file picker" })
 
-      -- colorscheme
+      keymap("n", "<leader>k", function()
+        vim.cmd("leftabove split")
+        require("fzf-lua").files()
+      end, { desc = "Split up and open file picker" })
+
+      keymap("n", "<leader>l", function()
+        vim.cmd("rightbelow vsplit")
+        require("fzf-lua").files()
+      end, { desc = "Split right and open file picker" })
+
+      -- COLORSCHEME
       require("kanagawa").load("wave")
 
-      -- git signs
-      require('gitsigns').setup()
+      -- AUTO PAIRS
+      require("nvim-autopairs").setup({
+        check_ts = true,
+      })
 
-      -- statusline
-      require('lualine').setup({
+      -- GIT SIGNS
+      require("gitsigns").setup()
+
+      -- STATUS LINE
+      require("lualine").setup({
         options = {
           component_separators = { left = "", right = "" },
           section_separators = { left = "", right = "" },
         },
         sections = {
           lualine_b = {
-            {'branch', icon = ""},
-            {'diff'},
+            { "branch", icon = "" },
+            { "diff" },
           },
           lualine_c = {
-            {'filename'},
-            {'diagnostics', symbols = { error = ' ', warn = ' ', info = ' ', hint = ' '}},
+            { "filename" },
+            {
+              "diagnostics",
+              symbols = {
+                error = " ",
+                warn = " ",
+                hint = " ",
+                info = " ",
+              }
+            },
           },
           lualine_x = {
-            {'filetype', icon = ""},
+            { "filetype", icon = "" },
           },
         },
       })
 
-      -- tree
-      require('neo-tree').setup()
+      -- FILE TREE
+      require("neo-tree").setup()
 
       vim.api.nvim_create_autocmd("VimEnter", {
+        desc = "Auto-open neo-tree on startup",
         callback = function()
           require("neo-tree.command").execute({ action = "show" })
-        end
+        end,
       })
 
-      -- lsp config
-      vim.lsp.config.nixd = {
-        cmd = { 'nixd' },
-        filetypes = { 'nix' },
+      -- TREESITTER
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+
+      -- LUA_LS CONFIGURATION
+      vim.lsp.config.lua_ls = {
+        cmd = { "lua-language-server" },
+        filetypes = { "lua" },
         single_file_support = true,
-        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              version = "LuaJIT",
+            },
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      }
+      vim.lsp.enable("lua_ls")
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.lua",
+        callback = function()
+          local file = vim.fn.expand("%:p")
+          vim.fn.system("stylua " .. vim.fn.shellescape(file))
+          vim.cmd("edit!")
+        end,
+      })
+
+
+      -- NIXD CONFIGURATION
+      vim.lsp.config.nixd = {
+        cmd = { "nixd" },
+        filetypes = { "nix" },
+        single_file_support = true,
         settings = {
           nixd = {
             nixpkgs = {
@@ -92,53 +176,73 @@
             formatting = {
               command = { "nixfmt" },
             },
+            options = {
+              nixos = {
+                expr = "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.desktop.options",
+              },
+              home_manager = {
+                expr = "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.desktop.options.home-manager.users.type.getSubOptions []",
+              },
+            },
           },
         },
       }
-      vim.lsp.enable('nixd')
+      vim.lsp.enable("nixd")
 
-      -- lsp diagnostic signs
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        desc = "Format Nix files on save",
+        pattern = "*.nix",
+        callback = function()
+          vim.lsp.buf.format()
+        end,
+      })
+
+      -- DIAGNOSTICS
       vim.diagnostic.config({
         signs = {
           text = {
             [vim.diagnostic.severity.ERROR] = "",
             [vim.diagnostic.severity.WARN] = "",
             [vim.diagnostic.severity.HINT] = "",
-            [vim.diagnostic.severity.INFO] = ""
-          }
-        }
+            [vim.diagnostic.severity.INFO] = "",
+          },
+        },
+        virtual_text = {
+          prefix = "",
+          spacing = 2,
+        },
+        underline = true,
+        severity_sort = true,
+        float = {
+          focusable = false,
+          style = "minimal",
+          border = "rounded",
+          source = "if_many",
+          header = "",
+          prefix = "",
+        },
       })
 
-      -- completion
-      vim.o.completeopt = 'menuone,noselect'
+      -- COMPLETION
+      vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
-      vim.api.nvim_create_autocmd('InsertEnter', {
-        callback = function()
-          vim.lsp.completion.enable(true, vim.lsp.get_active_clients()[1].id, 0, { autotrigger = true })
-        end
-      })
-
-      -- format on save
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*.nix",
-        callback = function()
-          vim.lsp.buf.format({ async = false })
+      vim.api.nvim_create_autocmd("LspAttach", {
+        desc = "Enable completion when LSP attaches",
+        callback = function(event)
+          vim.lsp.completion.enable(true, event.data.client_id, event.buf, { autotrigger = true })
         end,
-      })
-
-      -- treesitter
-      require('nvim-treesitter.configs').setup({
-        highlight = { enable = true },
-        indent = { enable = true },
       })
     '';
 
     extraPackages = with pkgs; [
+      lua-language-server
       nixd
       nixfmt-rfc-style
+      stylua
     ];
 
     plugins = with pkgs.vimPlugins; [
+      fzf-lua
       gitsigns-nvim
       kanagawa-nvim
       lualine-nvim
