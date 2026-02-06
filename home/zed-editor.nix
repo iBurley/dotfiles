ae6b04b25830433633c10f87bffab579d4452bf3
iBurley <10081177+iBurley@users.noreply.github.com>
2026-02-05 19:58:11 -0500

    formatting

--- home/zed-editor.nix
+++ home/zed-editor.nix
@@ -94,15 +94,15 @@
           ];
         };
       };
-      lsp = {
-        lua-language-server = {
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = [ "vim" ];
-              };
-            };
-          };
+      lsp.lua-language-server.settings.Lua = {
+        diagnostics = {
+          globals = [ "vim" ];
+        };
+        runtime = {
+          version = "LuaJIT";
+        };
+        telemetry = {
+          enable = false;
         };
       };
     };
