230f3295e1f182bdcbe1ccc16959d85c182f7ec2
iBurley <10081177+iBurley@users.noreply.github.com>
2026-03-06 23:46:40 -0500

    cleaned up push-to-talk script after testing

--- system/ptt.nix
+++ system/ptt.nix
@@ -6,15 +6,17 @@ let
   offSound = "${pkgs.sound-theme-freedesktop}/share/sounds/freedesktop/stereo/dialog-information.oga";
 
   pttScript = pkgs.writeShellScriptBin "wayland-ptt" ''
-    set -ux
+    set -e
     ${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 1
 
-    ${pkgs.evtest}/bin/evtest "${devicePath}" | while read line; do
+    ${pkgs.evtest}/bin/evtest "${devicePath}" | while IFS= read -r line; do
       if [[ "$line" == *"(KEY_F24), value 1"* ]]; then
         ${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 0
+        ${pkgs.procps}/bin/pkill pw-play || true
         ${pkgs.pipewire}/bin/pw-play "${onSound}" &
       elif [[ "$line" == *"(KEY_F24), value 0"* ]]; then
         ${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 1
+        ${pkgs.procps}/bin/pkill pw-play || true
         ${pkgs.pipewire}/bin/pw-play "${offSound}" &
       fi
     done
@@ -22,17 +24,16 @@ let
 in
 {
 
-  environment.systemPackages = with pkgs; [
-    evtest
-    sound-theme-freedesktop
-  ];
-
   services.udev.extraRules = ''
     KERNEL=="event*", ATTRS{name}=="PCsensor FootSwitch", MODE="0660", GROUP="input"
   '';
 
   systemd.user.services.wayland-ptt = {
     description = "System-wide Push-to-Talk";
+    after = [
+      "pipewire.service"
+      "wireplumber.service"
+    ];
     wantedBy = [ "graphical-session.target" ];
     partOf = [ "graphical-session.target" ];
 
