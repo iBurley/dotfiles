462e02aba4710dbaa4924a30959cf200d24dba64
iBurley <10081177+iBurley@users.noreply.github.com>
2026-03-18 22:29:53 -0400

    rename nautilus scripts to simplify home.file usage

--- home/home.nix
+++ home/home.nix
@@ -10,17 +10,9 @@
   home.username = "iburley";
   home.homeDirectory = "/home/iburley";
 
-  home.file = {
-    ".local/share/nautilus/scripts/Convert to GIF".source = ../files/nautilus-scripts/convert-to-gif.sh;
-    ".local/share/nautilus/scripts/Convert to JPG".source = ../files/nautilus-scripts/convert-to-jpg.sh;
-    ".local/share/nautilus/scripts/Convert to MP3".source = ../files/nautilus-scripts/convert-to-mp3.sh;
-    ".local/share/nautilus/scripts/Convert to MP4".source = ../files/nautilus-scripts/convert-to-mp4.sh;
-    ".local/share/nautilus/scripts/Convert to PDF".source = ../files/nautilus-scripts/convert-to-pdf.sh;
-    ".local/share/nautilus/scripts/Convert to PNG".source = ../files/nautilus-scripts/convert-to-png.sh;
-    ".local/share/nautilus/scripts/Upload to Imgur".source =
-      ../files/nautilus-scripts/upload-to-imgur.sh;
-    ".local/share/nautilus/scripts/Upload to Pastebin".source =
-      ../files/nautilus-scripts/upload-to-pastebin.sh;
+  home.file.".local/share/nautilus/scripts/" = {
+    source = ../files/nautilus-scripts;
+    recursive = true;
   };
 
   xdg.configFile."user-dirs.dirs".force = true;
