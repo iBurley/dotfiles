f96fdc2c2eb3f304aaea2aad634841b5bd85fb9f
iBurley <10081177+iBurley@users.noreply.github.com>
2026-06-03 04:13:01 -0400

    Zed really doesn't like files being 755

--- desktops/niri/dotfiles.nix
+++ desktops/niri/dotfiles.nix
@@ -8,12 +8,26 @@
       { ... }:
 
       {
+
         xdg.configFile = {
           "foot".source = ../../dotfiles/foot;
           "fuzzel".source = ../../dotfiles/fuzzel;
+          "fuzzel/scripts/power.sh" = {
+            source = ../../dotfiles/fuzzel/scripts/power.sh;
+            executable = true;
+          };
+          "fuzzel/scripts/sound-input.sh" = {
+            source = ../../dotfiles/fuzzel/scripts/sound-input.sh;
+            executable = true;
+          };
+          "fuzzel/scripts/sound-output.sh" = {
+            source = ../../dotfiles/fuzzel/scripts/sound-output.sh;
+            executable = true;
+          };
           "niri".source = ../../dotfiles/niri;
           "yazi".source = ../../dotfiles/yazi;
         };
+
       };
 
   };
